536641bf06404c8ca9a20e1efa2176d18aef0eb9
Dagonez <davidgonveg@gmail.com>
2024-01-03 20:22:52 +0100

    Comminting

--- Handwritten digit recognizer/train_digit_recognizer.py
+++ Handwritten digit recognizer/train_digit_recognizer.py
@@ -1,9 +1,36 @@
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D
+from keras.optimizers import Adam
 from keras import backend as K
+
+# Funciones base
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def visualize_learning_curve(H):
+  epochs = len(H.history["loss"])
+  plt.style.use("ggplot")
+  plt.figure()
+  plt.plot(np.arange(0, epochs), H.history["loss"], label="train_loss")
+  plt.plot(np.arange(0, epochs), H.history["val_loss"], label="val_loss")
+  plt.plot(np.arange(0, epochs), H.history["accuracy"], label="train_acc")
+  plt.plot(np.arange(0, epochs), H.history["val_accuracy"], label="val_acc")
+  plt.title("Training Loss and Accuracy")
+  plt.xlabel("Epoch #")
+  plt.ylabel("Loss/Accuracy")
+  plt.legend()
+
+from sklearn.metrics import classification_report
+
+def evaluate_model(model, x, y):
+  print("[INFO]: Evaluando red neuronal...")
+  predictions = model.predict(x, batch_size=128)
+  print(classification_report(y, predictions.argmax(axis=1)))
+
 
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -20,20 +47,24 @@ y_test = keras.utils.to_categorical(y_test, 10)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
+
+# Normalization
 x_train /= 255
 x_test /= 255
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-batch_size = 128
+batch_size = 512
 num_classes = 10
-epochs = 10
+epochs = 20
 
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(5, 5),activation='relu',input_shape=input_shape))
+model.add(Conv2D(32, kernel_size=(6, 6),activation='relu',input_shape=input_shape))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
@@ -42,15 +73,23 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
-model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
 
-hist = model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(x_test, y_test))
+hist = model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(x_test, y_test),shuffle=True)
 print("The model has successfully trained")
 
-score = model.evaluate(x_test, y_test, verbose=0)
+score = model.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
-model.save('mnist.h5')
+# Evaluación + Testeo
+
+# Learning curves
+visualize_learning_curve(hist)
+
+# Evaluando el modelo de predicción con las imágenes de test
+evaluate_model(model, x_test, y_test)
+
+#model.save('mnist.h5')
 print("Saving the model as mnist.h5")
 
